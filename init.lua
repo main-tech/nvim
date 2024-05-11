0ad830ee3afe4d50fd670444920aa136df6c1a97
Mandela Mitau <digitalcreative@mandelamitau.com>
2024-05-11 17:44:24 +0300

    Add shortcuts for discover projects and and nvim tree

--- init.lua
+++ init.lua
@@ -166,6 +166,8 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>S', '<cmd>Telescope neovim-project discover<CR>', { desc = 'Open/discover Projects' })
+vim.keymap.set('n', '<leader>E', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle nvim tree file explorer' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -330,7 +332,9 @@ require('lazy').setup({
       { 'nvim-tree/nvim-web-devicons' },
     },
     config = function()
-      require('nvim-tree').setup {}
+      require('nvim-tree').setup {
+        sync_root_with_cwd = true,
+      }
     end,
   },
   -- NOTE: Plugins can specify dependencies.
