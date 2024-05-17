713514aeb5d5514edce83695441269883ffb8062
Mandela Mitau <digitalcreative@mandelamitau.com>
2024-05-17 16:36:12 +0300

    add keymap for next and previous buffer and alo map 'jj' in insert mode to escape key

--- init.lua
+++ init.lua
@@ -177,12 +177,16 @@ vim.keymap.set('n', '<leader>E', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle nvi
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+vim.keymap.set('i', 'jj', '<ESC><ESC>', { desc = "Map 'jj'  to escape key " })
+
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+vim.keymap.set('n', '<A-.>', '<cmd>BufferPrevious<CR>')
+vim.keymap.set('n', '<A-,>', '<cmd>BufferNext<CR>')
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
