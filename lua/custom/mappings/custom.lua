a691af63ad55c890973b218999ee3b2cb886b07f
Mandela M Muithi <digitalcreative@mandelamitau.com>
2024-05-18 21:55:10 +0300

    add mapping for closing current buffer

--- lua/custom/mappings/custom.lua
+++ lua/custom/mappings/custom.lua
@@ -2,3 +2,4 @@ vim.keymap.set('n', '<F7>', '<cmd>ToggleTerm<CR>', { desc = 'Toggle nvim tree fi
 vim.keymap.set('n', '<A-.>', '<cmd>BufferNext<CR>')
 vim.keymap.set('n', '<A-,>', '<cmd>BufferPrevious<CR>')
 vim.keymap.set('n', '<leader>bC', '<cmd>%bdelete|edit #|normal`"<CR>', { desc = 'Delete all buffer except from the open one' })
+vim.keymap.set('n', '<leader>bc', '<cmd>BufferClose<CR>', { desc = 'Close current buffer' })
